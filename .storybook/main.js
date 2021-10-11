3884357bb4074691dd7f40828d76e5c44dc96512
jaya unyauni <jayaunyauni9999@gmail.com>
2021-10-11 07:26:26 +0000

    simple components added in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,10 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  stories: [ '../src/components/**/*.stories.js'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,7 @@
+import '../src/index.css';
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+
 };
