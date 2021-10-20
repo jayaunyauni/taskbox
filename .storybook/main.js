c5a7f87e579f00aff92362857acf85f9befd5579
jaya unyauni <jayaunyauni9999@gmail.com>
2021-10-20 04:58:00 +0000

    adding Screen component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
   stories: [ '../src/components/**/*.stories.js'],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
